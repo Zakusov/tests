0ecf80b8891ecc5f8e2eb3b8533609a0c9aaee72
Sergey Zakusov <sergey.zakusov@gmail.com>
2017-10-15 15:26:35 +0300

    Task 6

---
+++ src/test/java/ru/zakusov/test/it/Task06.java
@@ -0,0 +1,21 @@
+package ru.zakusov.test.it;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Task06 {
+
+    /**
+     * Реализуйте метод, который возвращает букву, стоящую в таблице UNICODE после символа "\" (обратный слэш) на расстоянии a.
+     */
+    public static char charExpression(int a) {
+        return (char) ('\\' + a);
+    }
+
+    @Test
+    public void charExpression() {
+        assertEquals('|', charExpression(32));
+        assertEquals('y', charExpression(29));
+    }
+}
