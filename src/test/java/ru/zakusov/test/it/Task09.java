17aac6c91f622ae9db876f8eb987a4fa6ec44682
Sergey Zakusov <sergey.zakusov@gmail.com>
2017-10-15 16:16:47 +0300

    Check simple questions

---
+++ src/test/java/ru/zakusov/test/it/Task09.java
@@ -0,0 +1,30 @@
+package ru.zakusov.test.it;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+public class Task09 {
+
+    @Test
+    public void testConcatenation() {
+        System.out.println('A');
+        System.out.println('A' + '1');
+        System.out.println('A' + '1' + "2");
+        assertNotEquals("A12", 'A' + '1' + "2");
+
+        assertEquals("A12", "A" + 12);
+
+        System.out.println('\t' + '\u0003');
+        assertEquals("A12", "A" + ('\t' + '\u0003'));
+
+        assertEquals("A12", 'A' + "12");
+    }
+
+    @Test
+    public void testArray() {
+        int[] array = new int[0];
+        assertEquals(0, array.length);
+    }
+}
