e5001a658853a06845b53bfe3f24d513b716b872
Sergey Zakusov <sergey.zakusov@gmail.com>
2017-10-30 10:28:09 +0300

    Sqrt

---
+++ src/main/java/ru/zakusov/test/chapter04/Sqrt.java
@@ -0,0 +1,20 @@
+package ru.zakusov.test.chapter04;
+
+/**
+ * Реализуйте метод sqrt(), вычисляющий квадратный корень числа. В отличие от Math.sqrt(),
+ * это метод при передаче отрицательного параметра должен бросать исключение java.lang.IllegalArgumentException
+ * с сообщением "Expected non-negative number, got ?", где вместо вопросика будет подставлено
+ * фактически переданное в метод число.
+ */
+public class Sqrt {
+
+    /**
+     * Контракт метода по заданию.
+     */
+    public static double sqrt(double x) {
+        if (x < 0) {
+            throw new IllegalArgumentException("Expected non-negative number, got " + x);
+        }
+        return Math.sqrt(x);
+    }
+}
---
+++ src/test/java/ru/zakusov/test/chapter04/SqrtTest.java
@@ -0,0 +1,25 @@
+package ru.zakusov.test.chapter04;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class SqrtTest {
+
+    @Test
+    public void sqrt9() {
+        assertEquals(3.0, Sqrt.sqrt(9), 1.0E-09);
+    }
+
+    @Test
+    public void sqrt0() {
+        assertEquals(3.0, Sqrt.sqrt(9), 1.0E-09);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void sqrtForNegativeValue() {
+        Sqrt.sqrt(-9);
+        fail("Expected an IllegalArgumentException");
+    }
+}
